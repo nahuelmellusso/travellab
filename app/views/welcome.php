4d5b9cabf53710aa29595f21f6c6d1bb724c8ce4
nahuelmellusso <nahuelmellusso@gmail.com>
2014-09-11 23:13:04 -0300

    fotos

--- app/views/welcome.php
+++ app/views/welcome.php
@@ -23,6 +23,51 @@
 
 
 
+    <div class="orbit-container">
+      <ul data-orbit class="example-orbit orbit-slides-container">
+        <li>
+          <img src="assets/img/irlanda2.jpg" alt="slide 1" />
+          <div class="orbit-caption">
+            Caption One.
+          </div>
+        </li>
+        <li class="active">
+          <img src="assets/img/irlanda.jpg" alt="slide 1" />
+          <div class="orbit-caption">
+            Caption Two.
+          </div>
+        </li>
+        <li>
+          <img src="assets/img/irlanda3.jpg" alt="slide 1" />
+          <div class="orbit-caption">
+            Caption Three.
+          </div>
+        </li>
+      </ul>
+
+      <!-- Navigation Arrows -->
+      <a href="#" class="orbit-prev">Prev <span></span></a>
+      <a href="#" class="orbit-next">Next <span></span></a>
+
+      <!-- Slide Numbers -->
+      <div class="orbit-slide-number">
+        <span>1</span> of <span>3</span>
+      </div>
+
+      <!-- Timer and Play/Pause Button -->
+      <div class="orbit-timer">
+        <span></span>
+        <div class="orbit-progress"></div>
+      </div>
+    </div>
+
+    <!-- Bullets -->
+    <ol class="orbit-bullets">
+      <li data-orbit-slide-number="1"></li>
+      <li data-orbit-slide-number="2" class="active"></li>
+      <li data-orbit-slide-number="3"></li>
+    </ol>
+
         
     <script src="assets/js/vendor/jquery.js"></script>
     <script src="assets/js/foundation.min.js"></script>
