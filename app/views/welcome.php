e8cdfcfb3076766e6d283cb80714c4324f797ec3
nahuelmellusso <nahuelmellusso@gmail.com>
2014-09-12 13:09:42 -0300

    fonts

--- app/views/welcome.php
+++ app/views/welcome.php
@@ -23,50 +23,13 @@
 
 
 
-    <div class="orbit-container">
-      <ul data-orbit class="example-orbit orbit-slides-container">
-        <li>
-          <img src="assets/img/irlanda2.jpg" alt="slide 1" />
-          <div class="orbit-caption">
-            Caption One.
-          </div>
-        </li>
-        <li class="active">
-          <img src="assets/img/irlanda.jpg" alt="slide 1" />
-          <div class="orbit-caption">
-            Caption Two.
-          </div>
-        </li>
-        <li>
-          <img src="assets/img/irlanda3.jpg" alt="slide 1" />
-          <div class="orbit-caption">
-            Caption Three.
-          </div>
-        </li>
-      </ul>
-
-      <!-- Navigation Arrows -->
-      <a href="#" class="orbit-prev">Prev <span></span></a>
-      <a href="#" class="orbit-next">Next <span></span></a>
-
-      <!-- Slide Numbers -->
-      <div class="orbit-slide-number">
-        <span>1</span> of <span>3</span>
-      </div>
-
-      <!-- Timer and Play/Pause Button -->
-      <div class="orbit-timer">
-        <span></span>
-        <div class="orbit-progress"></div>
+    <div class="row">
+      <div class="small-12 encontra">
+        <p class="p-en-negr">¿Ya sabes qu&eacute; quer&eacute;s hacer en tus vacaciones?</p>
+        <p class="p-en">Encontr&aacute; tu viaje</p>
+       
       </div>
     </div>
-
-    <!-- Bullets -->
-    <ol class="orbit-bullets">
-      <li data-orbit-slide-number="1"></li>
-      <li data-orbit-slide-number="2" class="active"></li>
-      <li data-orbit-slide-number="3"></li>
-    </ol>
 
         
     <script src="assets/js/vendor/jquery.js"></script>
