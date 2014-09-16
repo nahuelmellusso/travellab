57db60200600b12ed79d95378588904365ef4a86
nahuelmellusso <nahuelmellusso@gmail.com>
2014-09-16 16:12:08 -0300

    ahora si

---
+++ app/views/seguros.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Travel-lab</title>
+    <link rel="stylesheet" href="assets/css/foundation.css" />
+     <link rel="stylesheet" href="assets/css/estilo.css" />
+    <script src="assets/js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+
+  	<div class="row padding borde-bot">
+  	  	<div class="small-4 large-4  columns qs">Quienes somos</div>
+  	  	<div class="small-4 large-4 columns  borde-izquierda qs">Cont&aacute;ctenos</div>
+  	  	<div class="small-4 borde-izquierda columns">	
+  	  		<div class="row">
+  	  			<div class="small-6 large-6 columns " ><a class="fb"  href="#"></a></div>
+  	  			<div class="small-6 large-6  columns " ><a class="tw"  href="#"></a></div>
+  	  		</div>
+  	 	</div>	
+  	</div>
+
+  	<div class="row">
+  		<div class="small-12 columns menu">
+  			<div class="row collapse">
+
+  					<div class="small-2 small-offset-1 columns">
+  						<label class="menu-label">¿D&oacute;nde?</label>
+  					</div>
+  			        <div class="small-6 columns">			        
+  			          <input type="text"  placeholder="Ingresa un destino">
+  			        </div>
+  			        <div class="small-2 small-pull-1 columns put">
+  			          
+  			        </div>
+  			      </div>
+  		</div>
+  	</div>
+
+  	 <script src="assets/js/vendor/jquery.js"></script>
+    <script src="assets/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
